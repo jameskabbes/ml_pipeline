760b66f4a0abb1388ff3774cdd539425c9e40805
James Kabbes <james.kabbes@gmail.com>
2022-04-06 17:38:18 -0500

    standarzing main

--- src/ml_pipeline/__init__.py
+++ src/ml_pipeline/__init__.py
@@ -8,3 +8,11 @@ from . import ML_params
 from . import ML_ParentClass
 from . import ML_support
 from . import Templates
+
+import dir_ops.dir_ops as do
+import os
+
+Dir = do.Dir( os.path.abspath( __file__ ) ).ascend()   #Dir that contains the package 
+src_Dir = Dir.ascend()                                  #src Dir that is one above
+repo_Dir = src_Dir.ascend()                    
+cwd_Dir = do.Dir( do.get_cwd() )
--- src/ml_pipeline/__main__.py
+++ src/ml_pipeline/__main__.py
@@ -1,3 +1,6 @@
-print()
-print('In the future, this will run ml_pipeline for this directory')
-print()
+import sys
+sys_args = sys.argv[1:]
+
+from ml_pipeline.ml_pipeline import run
+run( *sys_args )
+
