810525dbe876b1203f38caf4f975c229b3be9f48
James Kabbes <james.kabbes@gmail.com>
2022-04-12 16:21:48 -0500

    adding ability to keep template files in the package

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        package_data={'ml_pipeline': 
+            [ 'Templates/*.ipynb', 'Templates/*.xlsx' ]
+            }
+    )
