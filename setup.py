ec04ec5bbd76ab07c1e612e5f10042826b40dc99
James Kabbes <james.kabbes@gmail.com>
2023-03-31 16:02:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         package_data={'ml_pipeline': 
-            [ 'Templates/*.ipynb', 'Templates/*.xlsx' ]
+            [ 'Templates/*.ipynb', 
+            'Templates/*.xlsx',
+            'CONFIG.json' ]
             }
     )
